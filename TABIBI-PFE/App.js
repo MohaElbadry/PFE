f66de3801342eb850c1caad6b5382f82041cd40d
mohammed el badry <mohammedbadry2003@gmail.com>
2023-02-12 03:56:05 +0100

    First-Design

--- TABIBI-PFE/App.js
+++ TABIBI-PFE/App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      className="flex-1 flex-col items-center  justify-center mt-4 "
+      style={{
+        backgroundColor: "#F6F6F6",
+      }}
+    >
+      <View
+        className=" mx-5 basis-2/3 items-center  justify-center w-full rounded-br-full shadow-md "
+        style={{
+          backgroundColor: "#ECF2F3",
+          borderBottomLeftRadius: 150,
+          borderBottomRightRadius: 150,
+        }}
+      >
+        <Text className="  font-extrabold text-3xl mt-24 ">Tabibi</Text>
+        <Image
+          source={require("./assets/Logo-Doc.png")}
+          className="h-2/3  w-80 from-neutral-50 drop-shadow-xl rounded-b-full"
+        />
+      </View>
+      <View className="basis-1/3 flex-col items-center  w-full ">
+        <Text className=" mt-7  font-bold">
+          Votre meilleur assistant médical
+        </Text>
+        <Text className="px-9 text-xs mt-5 ">
+          Dapatkan layanan medis spesial untuk kamu 24 jam.{" "}
+        </Text>
+        <TouchableOpacity className="absolute bottom-10 w-60 h-12 justify-center self-center mt-8 shadow-lg bg-cyan-600 rounded-lg">
+          <Text className=" self-center text-white text-xl font-bold">
+            Next{" "}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
